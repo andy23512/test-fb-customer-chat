2e49e0d516ed5edd37f264fb930fef18e208e53c
andy23512 <andy23512@gmail.com>
2018-08-14 15:50:54 +0800

    init with ctare, add facebook customer chat plugin

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
+import 'semantic-ui-reset/reset.css'
+import '@/assets/global.sass'
+
 import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
+import axios from 'axios'
+
+import App from '@/App.vue'
+import store from '@/store'
+
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
 new Vue({
   store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
